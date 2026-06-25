0d175ec34c4b24afefa18cd61ca07cf9154b42b3
Francesco Lacriola <francescolacriola@yahoo.com>
2026-06-25 20:16:00 +0200

    chore(songwriter): drop unused import in stretch rules test

--- test/schema/rules/songwriter_stretch_rules_test.dart
+++ test/schema/rules/songwriter_stretch_rules_test.dart
@@ -1,17 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:muzician/models/song_project.dart';
 import 'package:muzician/models/songwriter.dart';
 import 'package:muzician/schema/rules/songwriter_stretch_rules.dart';
 
 SongwriterProjectSnapshot _p(int tempo) => SongwriterProjectSnapshot(
-      config: SongwriterConfig(tempo: tempo, beatsPerBar: 4, beatUnit: 4),
-      audioClips: const [AudioClip(id: 'c1', assetId: 'a1', trimEndMs: 1000)],
-      sections: const [SongSection(id: 's1', lengthBars: 4, order: 0, lanes: [
-        SongLane(id: 'l1', kind: SongLaneKind.audio, order: 0, blocks: [
-          SongBlock(id: 'b1', startBar: 0, spanBars: 2, audioClipId: 'c1'),
-        ]),
-      ])],
-    );
+  config: SongwriterConfig(tempo: tempo, beatsPerBar: 4, beatUnit: 4),
+  audioClips: const [AudioClip(id: 'c1', assetId: 'a1', trimEndMs: 1000)],
+  sections: const [
+    SongSection(
+      id: 's1',
+      lengthBars: 4,
+      order: 0,
+      lanes: [
+        SongLane(
+          id: 'l1',
+          kind: SongLaneKind.audio,
+          order: 0,
+          blocks: [
+            SongBlock(id: 'b1', startBar: 0, spanBars: 2, audioClipId: 'c1'),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
 
 void main() {
   test('audioClipSpanBars finds the placing block span', () {
